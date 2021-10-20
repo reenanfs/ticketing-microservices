ee0f8326fdbed5822df6ed091e7b048ebac94c50
renanferreira <renan.sant@ibm.com>
2021-10-20 17:25:34 -0300

    jwt authentication implemented

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, {Request, Response} from 'express';
 import {body, validationResult} from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
@@ -32,6 +33,17 @@ router.post('/api/users/signup',
 
     const user = User.build({email, password});
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, process.env.JWT_KEY!);
+
+    // Store it on Session object
+    req.session = {
+        jwt: userJwt
+    }
 
     res.status(201).send(user);
 
