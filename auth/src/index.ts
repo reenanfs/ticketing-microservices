484a9d9d90f93a04c5ff938d4a2bbf3f827661d6
renanferreira <renan.sant@ibm.com>
2021-10-20 15:50:28 -0300

    added password hashing

---
+++ auth/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import 'express-async-errors'
+import { json } from 'body-parser'
+import mongoose from 'mongoose';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+
+const app = express();
+
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    } catch (err) {
+        console.error(err);
+    }
+    
+    app.listen(3000,() => {
+        console.log('Listening on port 3000!!');
+    });
+};
+
+start();
---
+++ auth/src/models/user.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+import {Password} from '../services/password';
+
+
+//interface describing properties required to create new user
+interface UserAttrs {
+    email: string;
+    password: string;
+};
+
+// interface describing properties a user model has
+interface UserModel extends mongoose.Model<Userdoc> {
+    build(attrs: UserAttrs): Userdoc;
+};
+
+//interface that describes the properties a single user has
+interface Userdoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+userSchema.pre('save', async function(done) {
+    if (this.isModified('password')) {
+        const hashed = await Password.toHash(this.get('password'));
+        this.set('password', hashed);
+    }
+    done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+    return new User(attrs);
+};
+
+const User = mongoose.model<Userdoc, UserModel>('User', userSchema);
+
+export {User};
