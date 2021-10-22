d4d9bcad7b9a7141363e9a60f07032db6d21d883
renanferreira <renan.sant@ibm.com>
2021-10-22 00:43:15 -0300

    completed testing on current-user

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,13 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 
-let mongo: any;
+declare global {
+    var signin: () => Promise<string[]>;
+}
 
+let mongo: any;
 beforeAll(async () => {
     process.env.JWT_KEY = 'dfgd'
 
@@ -26,3 +30,20 @@ afterAll(async() => {
     await mongo.stop();
     await mongoose.connection.close();
 });
+
+global.signin = async () => {
+    const email = 'test@test.com';
+    const password = 'password';
+
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email,
+            password
+        })
+        .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    return cookie;
+}
