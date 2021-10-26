db1fa0109a60041646339c913b65b01dcbf89f0a
renanferreira <renan.sant@ibm.com>
2021-10-26 16:57:54 -0300

    set mongo uri as env variable

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,8 +8,12 @@ const start = async () => {
         throw new Error('JWT_KEY must be defined');
     }
 
+    if (!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined');
+    }
+
     try {
-        await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets');
+        await mongoose.connect(process.env.MONGO_URI);
     } catch (err) {
         console.error(err);
     }
