a16b22db3c43016aba679014f2ffbe036b40ce5e
renanferreira <renan.sant@ibm.com>
2021-10-27 20:33:15 -0300

    added create ticket route and corresponding tests

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -3,7 +3,8 @@ import 'express-async-errors'
 import { json } from 'body-parser'
 import cookieSession from 'cookie-session';
 
-import { errorHandler } from '@reenanfs-ticketing/common';
+import { errorHandler, RouteNotFoundError, currentUser } from '@reenanfs-ticketing/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -14,7 +15,8 @@ app.use(cookieSession({
     secure: process.env.NODE_ENV !== 'test'
 })
 );
-
+app.use(currentUser);
+app.use(createTicketRouter);
 app.use(errorHandler);
 
 export { app };
