be38464e42dce687f27ade6f68eeff56db1acef9
renanferreira <renan.sant@ibm.com>
2022-01-21 22:01:32 -0300

    added starting message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,24 +3,25 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-    
-    if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined');
-    }
+  console.log('Starting...');
 
-    if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined');
-    }
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
 
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-    } catch (err) {
-        console.error(err);
-    }
-    
-    app.listen(3000,() => {
-        console.log('Listening on port 3000!!');
-    });
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined');
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!');
+  });
 };
 
 start();
